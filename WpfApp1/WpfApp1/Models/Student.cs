8ab992748f4c391ecae7f837f6e169dbfe4dc210
Валера Коршун <korshunval2033@gmail.com>
2021-04-15 17:11:57 +0300

    Fix Student

--- WpfApp1/WpfApp1/Models/Student.cs
+++ WpfApp1/WpfApp1/Models/Student.cs
@@ -10,6 +10,8 @@ namespace WpfApp1.Models
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
     }
 }
